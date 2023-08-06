2c6294de1933fce870ec9ec45f7e49973fb92318
Thiago Sousa <thiagosousa81@gmail.com>
2023-08-06 01:38:23 +0000

    modified:   index.php 	modified:   login/index.html

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clinfomed - Painel</title>
+    <link rel="shortcut icon" href="/src/logo.jpg"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css?_cacheOverride=1691177712738" rel="stylesheet">
 </head>
 <body>
     
