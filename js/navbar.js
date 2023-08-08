1dfd52c8646eb2582d7233dedc1d42d5e1823103
Thiago Sousa <thiagosousa81@gmail.com>
2023-08-08 18:08:14 +0000

    modified:   index.html 	new file:   js/navbar.js

---
+++ js/navbar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return ( 
+        <div class="p-4">
+            <h4 class="text-white">Menu</h4>
+            <ul class="list-unstyled">
+                <li><a href="#" class="text-white btn btn-secondary">CONSULTAS</a></li>
+                <li><a href="#" class="text-white btn btn-secondary">PACIENTES</a></li>
+                <li><a href="#" class="text-white btn btn-secondary">MÉDICOS</a></li>
+                <li><a href="#" class="text-white btn btn-secondary">FUNCIONÁRIOS</a></li>
+                <li><a href="#" class="text-white btn btn-secondary">CONFIGURAÇÕES</a></li>
+            </ul>
+        </div>
+    );
+}
